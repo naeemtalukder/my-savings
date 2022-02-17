4fd2fd01f967c4ee04a778ec036db501c3097cc4
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-02-17 12:19:03 +0600

    final commit

--- scripte/script.js
+++ scripte/script.js
@@ -1,3 +1,4 @@
+// fetInputValue function
 function getInputValue(inputId) {
     const inputValue = document.getElementById(inputId);
     const inputValueTaxt = inputValue.value;
@@ -5,11 +6,25 @@ function getInputValue(inputId) {
     return inputValueNumber;
 }
 
+// Calculate part
 document.getElementById('calculate').addEventListener('click', function () {
     const incomeInput = getInputValue('income-input');
+    if (isNaN(incomeInput) == incomeInput > 0) {
+        return alert('plaese input valid amount')
+    }
+
     const foodInput = getInputValue('food-input');
+    if (isNaN(foodInput) == foodInput > 0) {
+        return alert('plaese input valid amount')
+    }
     const rentInput = getInputValue('rent-input');
+    if (isNaN(rentInput) == rentInput > 0) {
+        return alert('plaese input valid amount')
+    }
     const clothesInput = getInputValue('clothes-input');
+    if (isNaN(clothesInput) == clothesInput > 0) {
+        return alert('plaese input valid amount')
+    }
     const expenses = document.getElementById('total-expenses');
 
     const totalExpenses = foodInput + rentInput + clothesInput;
@@ -20,13 +35,19 @@ document.getElementById('calculate').addEventListener('click', function () {
     balance.innerText = totalBalance;
 
 });
+
+//Saving calculate part
 document.getElementById('save-btn').addEventListener('click', function () {
     const incomeInput = getInputValue('income-input');
     const saveInput = getInputValue('save-input');
+    if (isNaN(saveInput) == saveInput > 0) {
+        return alert('plaese input valid amount')
+    }
+
     const balance = document.getElementById('balance').innerText;
     const saveAmount = document.getElementById('saving-amount').innerText = incomeInput * saveInput / 100;
 
     const remainingBalanceText = document.getElementById('remaining-balance').innerText = balance - saveAmount;
     const remainingBalance = parseFloat(remainingBalanceText.innerText);
     console.log(remainingBalance);
-})
+});
